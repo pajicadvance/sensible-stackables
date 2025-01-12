ba02ebdf6055a1efe95b175ed5b7f92b4671b436
pajicadvance <pavle.pajic23@gmail.com>
2025-01-12 15:15:40 +0100

    neoforge mod

---
+++ src/main/java/me/pajic/sensible_stackables/config/ModConfig.java
@@ -0,0 +1,79 @@
+package me.pajic.sensible_stackables.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import net.neoforged.fml.loading.FMLPaths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class ModConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("SensibleStackables-Config");
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final Path configFile = FMLPaths.CONFIGDIR.get().resolve("sensible-stackables.json");
+    public static Config CONFIG;
+
+    public static class Config {
+        public boolean enableStackablePotions = true;
+        public int potionMaxStackSize = 3;
+        public int splashPotionCooldown = 1;
+        public boolean enableStackableEnchantedBooks = true;
+        public int enchantedBookMaxStackSize = 64;
+        public boolean enableStackableHorseArmor = true;
+        public int horseArmorMaxStackSize = 64;
+        public boolean enableStackableMusicDiscs = true;
+        public int musicDiscMaxStackSize = 64;
+        public boolean enableStackableSaddles = true;
+        public int saddleMaxStackSize = 16;
+        public boolean enableStackableMinecarts = true;
+        public int minecartMaxStackSize = 16;
+        public boolean enableStackableBoats = true;
+        public int boatMaxStackSize = 16;
+        public boolean enableStackableBeds = true;
+        public int bedMaxStackSize = 16;
+        public boolean enableStackableSnowballs = true;
+        public int snowballMaxStackSize = 64;
+        public boolean enableStackableEggs = true;
+        public int eggMaxStackSize = 64;
+    }
+
+    public static void loadConfig() {
+        readConfig();
+        saveConfig();
+    }
+
+    private static void readConfig() {
+        try (FileReader reader = new FileReader(configFile.toFile())) {
+            CONFIG = GSON.fromJson(reader, Config.class);
+        } catch (FileNotFoundException | JsonSyntaxException e) {
+            LOGGER.warn("Config doesn't exist or is malformed, initializing new mod config...");
+            initializeConfig();
+        } catch (IOException e) {
+            LOGGER.error("Failed to read mod config", e);
+        }
+    }
+
+    private static void initializeConfig() {
+        try (FileWriter writer = new FileWriter(configFile.toFile())) {
+            CONFIG = new Config();
+            GSON.toJson(CONFIG, writer);
+        } catch (IOException e) {
+            LOGGER.error("Failed to initialize mod config", e);
+        }
+    }
+
+    private static void saveConfig() {
+        try (FileWriter writer = new FileWriter(configFile.toFile())) {
+            GSON.toJson(CONFIG, writer);
+        } catch (IOException e) {
+            LOGGER.error("Failed to save mod config", e);
+        }
+    }
+}
---
+++ src/main/java/me/pajic/sensible_stackables/mixin/ItemMixin.java
@@ -0,0 +1,69 @@
+package me.pajic.sensible_stackables.mixin;
+
+import me.pajic.sensible_stackables.config.ModConfig;
+import net.minecraft.core.component.DataComponentMap;
+import net.minecraft.core.component.DataComponentPatch;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.component.PatchedDataComponentMap;
+import net.minecraft.world.item.*;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Item.class)
+public class ItemMixin {
+
+    @Mutable @Shadow private DataComponentMap components;
+
+    @Inject(
+            method = "<init>",
+            at = @At("RETURN")
+    )
+    private void modifyDataComponents(Item.Properties properties, CallbackInfo ci) {
+        Item item = (Item) (Object) this;
+        int newStackSize = -1;
+
+        if (ModConfig.CONFIG.enableStackablePotions && item instanceof PotionItem) {
+            newStackSize = ModConfig.CONFIG.potionMaxStackSize;
+        }
+        else if (ModConfig.CONFIG.enableStackableSaddles && item instanceof SaddleItem) {
+            newStackSize = ModConfig.CONFIG.saddleMaxStackSize;
+        }
+        else if (ModConfig.CONFIG.enableStackableMinecarts && item instanceof MinecartItem) {
+            newStackSize = ModConfig.CONFIG.minecartMaxStackSize;
+        }
+        else if (ModConfig.CONFIG.enableStackableBoats && item instanceof BoatItem) {
+            newStackSize = ModConfig.CONFIG.boatMaxStackSize;
+        }
+        else if (ModConfig.CONFIG.enableStackableBeds && item instanceof BedItem) {
+            newStackSize = ModConfig.CONFIG.bedMaxStackSize;
+        }
+        else if (ModConfig.CONFIG.enableStackableSnowballs && item instanceof SnowballItem) {
+            newStackSize = ModConfig.CONFIG.snowballMaxStackSize;
+        }
+        else if (ModConfig.CONFIG.enableStackableEggs && item instanceof EggItem) {
+            newStackSize = ModConfig.CONFIG.eggMaxStackSize;
+        }
+        else if (ModConfig.CONFIG.enableStackableHorseArmor && item instanceof AnimalArmorItem && !components.has(DataComponents.MAX_DAMAGE)) {
+            newStackSize = ModConfig.CONFIG.horseArmorMaxStackSize;
+        }
+        else if (ModConfig.CONFIG.enableStackableEnchantedBooks &&
+                //? if <= 1.21.1
+                item instanceof EnchantedBookItem
+                //? if > 1.21.1
+                /*components.has(DataComponents.STORED_ENCHANTMENTS) && !components.has(DataComponents.MAX_DAMAGE) && item.getDescriptionId().equals("item.minecraft.enchanted_book")*/
+        ) {
+            newStackSize = ModConfig.CONFIG.enchantedBookMaxStackSize;
+        }
+        else if (ModConfig.CONFIG.enableStackableMusicDiscs && components.has(DataComponents.JUKEBOX_PLAYABLE)) {
+            newStackSize = ModConfig.CONFIG.musicDiscMaxStackSize;
+        }
+
+        if (newStackSize > 0 && newStackSize <= 64) {
+            components = PatchedDataComponentMap.fromPatch(components, DataComponentPatch.builder().set(DataComponents.MAX_STACK_SIZE, newStackSize).build());
+        }
+    }
+}
