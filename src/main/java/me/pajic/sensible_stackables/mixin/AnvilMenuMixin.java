df54e45cf4b1a7710393e6d3e835fc03f53240c6
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-05-10 16:57:58 +0200

    Refresh anvil result on take

--- src/main/java/me/pajic/sensible_stackables/mixin/AnvilMenuMixin.java
+++ src/main/java/me/pajic/sensible_stackables/mixin/AnvilMenuMixin.java
@@ -1,18 +1,24 @@
 package me.pajic.sensible_stackables.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.*;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(AnvilMenu.class)
 public abstract class AnvilMenuMixin extends ItemCombinerMenu {
+
+    @Shadow public abstract void createResult();
 
     //? if <= 1.21.1 {
     public AnvilMenuMixin(@Nullable MenuType<?> type, int containerId, Inventory playerInventory, ContainerLevelAccess access) {
@@ -87,5 +93,11 @@ public abstract class AnvilMenuMixin extends ItemCombinerMenu {
             return stack;
         }
         return original;
+    }
+
+    @WrapMethod(method = "onTake")
+    private void triggerCreateResultOnTake(Player player, ItemStack stack, Operation<Void> original) {
+        original.call(player, stack);
+        createResult();
     }
 }
